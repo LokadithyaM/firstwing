ee84af7e36748ca5b277f08aafd60978cc69e6bf
LokadithyaM <lokad1thya.m@gmail.com>
2023-04-22 08:51:46 +0530

    version.02

--- prototype1.java
+++ prototype1.java
@@ -3,31 +3,45 @@ import java.util.Scanner;
 
 class setup{
     int x;
+    int e[]={1,2,3,4,5,6};
     private String a ="JAVA";
     private String b ="Trojan";
     private String c ="google";
     ArrayList<String> my_list = new ArrayList<String>();
          
 
-    void setup(){
+     private void setting_up(){
          my_list.add(a);
          my_list.add(b);
          my_list.add(c);
+         System.out.println(my_list);
     }
-
-    void verify(){
-        String s[]={"A1","A2","A3","A4","A5","A6","A7"};
-        System.out.println(my_list);
+    String verify(int a){
+        return null;
     }
-
+    void caller(){
+        setting_up();
+    }
+    
 }
 public class prototype1{
         public static void main(String args[]){
+            int i=1;
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
         
         setup s = new setup();
-        s.setup();
-        s.verify();
+        s.caller();
+
+
+        while(i>0){
+            System.out.print("Your guess: ");
+            int a = sc.nextInt();
+            String y = s.verify(a);
+            if(y=="Hit"){
+                System.out.println("You got that right");
+            }
+            i++;
+        }
+        
     }
 }
